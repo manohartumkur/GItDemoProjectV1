cf5643331ed6f12d54375e958035f483a98dd39d
Manohara <manohara.ph@finonyx.com>
2021-12-04 21:52:30 +0530

    First Commit

---
+++ src/com/fin/Test.java
@@ -0,0 +1,14 @@
+package com.fin;
+
+public class Test {
+
+	public static void main(String[] args) {
+		String hungry = "yes";
+
+		if (hungry.equals("yes"))
+			System.out.println("I will eat something");
+		else
+			System.out.println(" DO NOTHING");
+	}
+
+}
